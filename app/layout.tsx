782eac1f43b4fe957789c4579d0aff77f4d78dcb
Sanika03 <sanikasuryawanshi0305@gmail.com>
2026-05-15 22:07:13 +0530

    Fix import path

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Providers from "./providers/providers";
+import Providers from "@/providers/providers";
 
 export const metadata: Metadata = {
   title: "Unstuck AI",
